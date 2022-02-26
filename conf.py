64657229fac88c597a920eb885c96c307cf2bd51
Brad Miller <brad@runestoneinteractive.com>
2022-02-26 00:29:19 +0000

    add bookshelf metadata

--- conf.py
+++ conf.py
@@ -212,6 +212,7 @@ html_theme_path = [pkg_resources.resource_filename('runestone', 'common/project_
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
 html_title = 'Mastering Relational Databases'
+shelf_section = 'Database'
 
 # A shorter title for the navigation bar.  Default is the same as html_title.
 html_short_title ='Mastering Databases'
